aa0cc1e6c178888b8b26c9cf03b0f931f742189f
태후\SMART <xogn13@ajou.ac.kr>
2025-07-23 22:24:53 +0900

    feat: MFC 다중 날짜 선택 기능 구현 및 UI/UX 강화
    - Month Calendar 컨트롤 기반 복수 날짜 선택 구현
    - 선택된 날짜를 std::set으로 관리하고 리스트박스에 실시간 표시
    - 근무형태 콤보박스 변경에 따라 방학 기간 DateTimePicker 컨트롤 활성/비활성 처리 추가
    - 초기 다이얼로그 설정 시 방학 날짜 컨트롤 비활성화 및 결과 텍스트 포맷 수정
    - DoDataExchange 내 불필요한 입력 처리 제거 및 컨트롤 변수 연결 코드 정리
    - 관련 UI 엘리먼트 초기화 및 이벤트 핸들러 등록 강화
    - 사용자 선택 편의성 및 코드 가독성 개선을 위한 구조화 작업 수행

--- AnnualLeaveApplication/CAnnualLeaveDlg.cpp
+++ AnnualLeaveApplication/CAnnualLeaveDlg.cpp
@@ -30,15 +30,14 @@ void CAnnualLeaveDlg::DoDataExchange(CDataExchange* pDX)
     DDX_Control(pDX, IDC_COMBO_VACATION_WORK, m_comboWorkType);
     DDX_Control(pDX, IDC_COMBO_WORK_SITE, m_comboWorkplace);
     DDX_Control(pDX, IDC_DATETIMEPICKER_WORK_START_DATE, m_dateStartDate);
+    DDX_Control(pDX, IDC_DATETIMEPICKER_SUMMER_VACATION_START, m_dateSummerVacationStart);
+    DDX_Control(pDX, IDC_DATETIMEPICKER_SUMMER_VACATION_END, m_dateSummerVacationEnd);
+	DDX_Control(pDX, IDC_DATETIMEPICKER_WINTER_VACATION_START, m_dateWinterVacationStart);
+	DDX_Control(pDX, IDC_DATETIMEPICKER_WINTER_VACATION_END, m_dateWinterVacationEnd);
     DDX_Control(pDX, IDC_STATIC_RESULT, m_staticResult);
+    DDX_Control(pDX, IDC_MONTHCALENDAR1, m_calendar);
+    DDX_Control(pDX, IDC_LIST_CUSTOMIZING_DAYS, m_listBox);
 
-    // 선택된 값 저장(입력받기)
-    if (!pDX->m_bSaveAndValidate)
-    {
-        m_nWorkTypeSel = m_comboWorkType.GetCurSel();
-        m_nWorkplaceSel = m_comboWorkplace.GetCurSel();
-        m_dateStartDate.GetTime(m_ctStartDate);
-    }
 }
 
 
@@ -47,6 +46,11 @@ BEGIN_MESSAGE_MAP(CAnnualLeaveDlg, CDialogEx)
     ON_CBN_SELCHANGE(IDC_COMBO_WORK_SITE, &CAnnualLeaveDlg::OnCbnSelchangeComboWorkSite)
     ON_NOTIFY(DTN_DATETIMECHANGE, IDC_DATETIMEPICKER_WORK_START_DATE, &CAnnualLeaveDlg::OnDtnDatetimechangeDatetimepickerWorkStartDate)
     ON_BN_CLICKED(IDC_BUTTON_CALCULATE, &CAnnualLeaveDlg::OnBnClickedButtonCalculate)
+    ON_NOTIFY(MCN_SELCHANGE, IDC_MONTHCALENDAR1, &CAnnualLeaveDlg::OnMcnSelchangeMonthcalendar)
+    ON_NOTIFY(DTN_DATETIMECHANGE, IDC_DATETIMEPICKER_SUMMER_VACATION_START, &CAnnualLeaveDlg::OnDtnDatetimechangeDatetimepickerSummerVacationStart)
+    ON_NOTIFY(DTN_DATETIMECHANGE, IDC_DATETIMEPICKER_SUMMER_VACATION_END, &CAnnualLeaveDlg::OnDtnDatetimechangeDatetimepickerSummerVacationEnd)
+    ON_NOTIFY(DTN_DATETIMECHANGE, IDC_DATETIMEPICKER_WINTER_VACATION_START, &CAnnualLeaveDlg::OnDtnDatetimechangeDatetimepickerWinterVacationStart)
+    ON_NOTIFY(DTN_DATETIMECHANGE, IDC_DATETIMEPICKER_WINTER_VACATION_END, &CAnnualLeaveDlg::OnDtnDatetimechangeDatetimepickerWinterVacationEnd)
 END_MESSAGE_MAP()
 
 
@@ -66,6 +70,8 @@ BOOL CAnnualLeaveDlg::OnInitDialog()
     m_comboWorkplace.AddString(_T("학교"));
     m_comboWorkplace.SetCurSel(0);
 
+    SetVacationDateControlsEnable(FALSE);
+
     // 날짜 선택기 초기값 현재 날짜로 설정
     CTime curTime = CTime::GetCurrentTime();
     SYSTEMTIME sysTime;
@@ -75,7 +81,7 @@ BOOL CAnnualLeaveDlg::OnInitDialog()
     m_ctStartDate = curTime;
 
     // 초기 결과 텍스트 공백
-    m_staticResult.SetWindowText(_T(""));
+    m_staticResult.SetWindowText(_T("근무지: \n근무형태: \n계산된 연차 일수: 일\n\n%s"));
 
     return TRUE;
 }
@@ -83,6 +89,16 @@ void CAnnualLeaveDlg::OnCbnSelchangeComboVacationWork()
 {
     // TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
     m_nWorkTypeSel = m_comboWorkType.GetCurSel();
+
+    // "비상시근무자"(1) 일 때만 방학 날짜 컨트롤 활성화
+    if (m_nWorkTypeSel == 1)
+    {
+        SetVacationDateControlsEnable(TRUE);
+    }
+    else
+    {
+        SetVacationDateControlsEnable(FALSE);
+    }
 }
 
 void CAnnualLeaveDlg::OnCbnSelchangeComboWorkSite()
@@ -153,4 +169,90 @@ void CAnnualLeaveDlg::OnBnClickedButtonCalculate()
         strStandardDate);
 
     m_staticResult.SetWindowText(strResult);
+}
+
+void CAnnualLeaveDlg::OnMcnSelchangeMonthcalendar(NMHDR* pNMHDR, LRESULT* pResult)
+{
+    SYSTEMTIME st;
+    if (m_calendar.GetCurSel(&st)) // 현재 선택된 날짜 얻기
+    {
+        COleDateTime clickedDate(st);
+
+        // 이미 선택한 날짜인지 확인
+        auto it = m_selectedDates.find(clickedDate);
+        if (it != m_selectedDates.end()) {
+            // 이미 있으면 해제(삭제)
+            m_selectedDates.erase(it);
+        }
+        else {
+            // 없으면 추가
+            m_selectedDates.insert(clickedDate);
+        }
+
+        UpdateSelectedDateList();
+    }
+
+    *pResult = 0;
+}
+
+void CAnnualLeaveDlg::UpdateSelectedDateList()
+{
+    m_listBox.ResetContent();
+
+    for (const auto& dt : m_selectedDates)
+    {
+        CString strDate = dt.Format(_T("%Y-%m-%d"));
+        m_listBox.AddString(strDate);
+    }
+}
+
+void CAnnualLeaveDlg::OnDtnDatetimechangeDatetimepickerSummerVacationStart(NMHDR* pNMHDR, LRESULT* pResult)
+{
+    LPNMDATETIMECHANGE pDTChange = reinterpret_cast<LPNMDATETIMECHANGE>(pNMHDR);
+    // TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+    *pResult = 0;
+}
+
+void CAnnualLeaveDlg::OnDtnDatetimechangeDatetimepickerSummerVacationEnd(NMHDR* pNMHDR, LRESULT* pResult)
+{
+    LPNMDATETIMECHANGE pDTChange = reinterpret_cast<LPNMDATETIMECHANGE>(pNMHDR);
+    // TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+    *pResult = 0;
+}
+
+void CAnnualLeaveDlg::OnDtnDatetimechangeDatetimepickerWinterVacationStart(NMHDR* pNMHDR, LRESULT* pResult)
+{
+    LPNMDATETIMECHANGE pDTChange = reinterpret_cast<LPNMDATETIMECHANGE>(pNMHDR);
+    // TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+    *pResult = 0;
+}
+
+void CAnnualLeaveDlg::OnDtnDatetimechangeDatetimepickerWinterVacationEnd(NMHDR* pNMHDR, LRESULT* pResult)
+{
+    LPNMDATETIMECHANGE pDTChange = reinterpret_cast<LPNMDATETIMECHANGE>(pNMHDR);
+    // TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+    *pResult = 0;
+}
+
+// 방학 날짜 컨트롤 활성/비활성 토글 함수
+void CAnnualLeaveDlg::SetVacationDateControlsEnable(BOOL bEnable)
+{
+    m_dateSummerVacationStart.EnableWindow(bEnable);
+    m_dateSummerVacationEnd.EnableWindow(bEnable);
+    m_dateWinterVacationStart.EnableWindow(bEnable);
+    m_dateWinterVacationEnd.EnableWindow(bEnable);
+
+    // 선택 해제 및 초기화 추가 (필요하면)
+    if (!bEnable)
+    {
+        // 초기값 현재 날짜 또는 빈값 넣기 (원하는 초기화 로직으로 수정 가능)
+        CTime curTime = CTime::GetCurrentTime();
+        SYSTEMTIME sysTime;
+        curTime.GetAsSystemTime(sysTime);
+
+        m_dateSummerVacationStart.SetTime(&sysTime);
+        m_dateSummerVacationEnd.SetTime(&sysTime);
+        m_dateWinterVacationStart.SetTime(&sysTime);
+        m_dateWinterVacationEnd.SetTime(&sysTime);
+    }
 }
--- AnnualLeaveApplication/CAnnualLeaveDlg.h
+++ AnnualLeaveApplication/CAnnualLeaveDlg.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #include "afxdialogex.h"
-
+#include <set>
+#include <afxdtctl.h>
 
 // CAnnualLeaveDlg 대화 상자
 
@@ -23,10 +24,18 @@ protected:
 	DECLARE_MESSAGE_MAP()
 
 private:
+	CMonthCalCtrl m_calendar;      // 달력 컨트롤
+	CListBox m_listBox;            // 선택 날짜 리스트박스
+	std::set<COleDateTime> m_selectedDates; // 선택된 날짜 저장용 set
 
 	CComboBox m_comboWorkType;      // 방학중 근무 여부 콤보박스
 	CComboBox m_comboWorkplace;     // 근무지 콤보박스
 	CDateTimeCtrl m_dateStartDate;  // 근무시작일 날짜 선택기
+
+	CDateTimeCtrl m_dateSummerVacationStart; // 여름방학 시작일
+	CDateTimeCtrl m_dateSummerVacationEnd;   // 여름방학 종료일
+	CDateTimeCtrl m_dateWinterVacationStart; // 겨울방학 시작일
+	CDateTimeCtrl m_dateWinterVacationEnd;   // 겨울방학 종료일
 
 	CStatic m_staticResult;          // 근속 결과 표시용 Static 텍스트
 
@@ -39,4 +48,13 @@ public:
 	afx_msg void OnCbnSelchangeComboWorkSite();
 	afx_msg void OnDtnDatetimechangeDatetimepickerWorkStartDate(NMHDR* pNMHDR, LRESULT* pResult);
 	afx_msg void OnBnClickedButtonCalculate();
+	afx_msg void OnMcnSelchangeMonthcalendar(NMHDR* pNMHDR, LRESULT* pResult);
+
+	void UpdateSelectedDateList();
+	void SetVacationDateControlsEnable(BOOL bEnable);
+
+	afx_msg void OnDtnDatetimechangeDatetimepickerSummerVacationStart(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnDtnDatetimechangeDatetimepickerSummerVacationEnd(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnDtnDatetimechangeDatetimepickerWinterVacationStart(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnDtnDatetimechangeDatetimepickerWinterVacationEnd(NMHDR* pNMHDR, LRESULT* pResult);
 };
